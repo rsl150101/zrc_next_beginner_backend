ef7b64e07e861a382be51d3c014962438145d1fa
rsl150101 <dnwnd501@naver.com>
2025-03-08 19:17:11 +0900

    [Add] 5-7 sequelize sync and automate connection by importing model files

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,18 @@
 import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+import db from "./models";
 
 const app = express();
+
+db.sequelize
+  ?.sync()
+  .then(() => {
+    console.log("Connect DB");
+  })
+  .catch(console.error);
 
 app.set("port", process.env.PORT || 3065);
 
--- src/models/index.ts
+++ src/models/index.ts
@@ -1,15 +1,21 @@
 import { Sequelize } from "sequelize";
+import path from "path";
+import fs from "fs";
+
 import config from "../config/config";
-import { initUser } from "./user";
-import { initPost } from "./post";
-import { initComment } from "./comment";
-import { initHashtag } from "./hashtag";
-import { initImage } from "./image";
 
 type Env = "development" | "test" | "production";
 
+interface IDB {
+  [key: string]: any;
+  sequelize?: Sequelize;
+}
+
 const env: Env = (process.env.NODE_ENV as Env) || "development";
 const configEnv = config[env];
+const db: IDB = {};
+const basename = path.basename(__filename);
+const fileExtension = process.env.NODE_ENV === "production" ? ".js" : ".ts";
 
 const sequelize = new Sequelize(
   configEnv.database,
@@ -18,10 +24,27 @@ const sequelize = new Sequelize(
   configEnv
 );
 
-initUser(sequelize);
-initPost(sequelize);
-initComment(sequelize);
-initHashtag(sequelize);
-initImage(sequelize);
+fs.readdirSync(__dirname)
+  .filter((file) => {
+    return (
+      file.indexOf(".") !== 0 &&
+      file !== basename &&
+      file.slice(-3) === fileExtension &&
+      file.indexOf(".test.ts") === -1
+    );
+  })
+  .forEach((file) => {
+    const model = require(path.join(__dirname, file));
+    db[model.name] = model;
+    model.default.initiate(sequelize);
+  });
 
-export default sequelize;
+Object.keys(db).forEach((modelName) => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+
+export default db;
